3643625e47259e6cb9a9d60055b1616266dbe3c1
Raghu <Raghu@Raghu-PC>
2020-12-05 13:49:29 +0530

    update done

--- src/test/java/com/project/Project3.java
+++ src/test/java/com/project/Project3.java
@@ -3,7 +3,7 @@ package com.project;
 public class Project3 {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to project3");
+		System.out.println("welcome to project3 successfully.......");
 	}
 
 }
